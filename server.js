5cc05d5a569140975b1dbac088d34050c3cb5062
Sarvesh Mokhare <sarvesh.ironman@gmail.com>
2022-11-15 17:28:02 +0530

    production ready

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// const path = require("path");
+const path = require("path");
 
 const connect = require('./config/connectToDB');
 const user = require("./routes/user");
@@ -28,18 +28,18 @@ app.use("/api/message", message);
 
 //------------------------------ deployment ---------------------------------
 
-// const __dirname1 = path.resolve();
+const __dirname1 = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname1, "/frontend/build")));
-//     app.get("*", (req, res) =>
-//         res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//     );
-// } else {
-//     app.get("/", (req, res) => {
-//         res.send("Request to root route ran successfully.");
-//     });
-// }
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("Request to root route ran successfully.");
+    });
+}
 
 //------------------------------ deployment ---------------------------------
 
